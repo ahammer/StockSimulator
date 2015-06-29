f5e3286b23ffaa7f49f81bea516ce2803a8f1535
Adam Hammer <adamhammer2@gmail.com>
2015-06-28 19:59:21 -0700

    updates to main menu

--- core/src/com/metalrain/stocksimulator/StockSimulatorTextEdition.java
+++ core/src/com/metalrain/stocksimulator/StockSimulatorTextEdition.java
@@ -6,7 +6,7 @@ import com.metalrain.stocksimulator.textClient.TextClient;
 /**
  * Created by Adam Hammer on 6/22/2015.
  */
-public class crimsim_text {
+public class StockSimulatorTextEdition {
     public static void main(String ... args) {
         System.out.println("Welcome to Crimsim Text Mode");
         GameState state = new GameState((int) (Math.random()*10000), 5000);
